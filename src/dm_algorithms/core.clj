e67af465789cf2ca5ef6178cd83f47d986f20471
TobiasRitter <tobias.ritter98@gmail.com>
2020-07-03 22:55:02 +0200

    perform linreg in core.clj

--- src/dm_algorithms/core.clj
+++ src/dm_algorithms/core.clj
@@ -1,7 +1,14 @@
 (ns dm-algorithms.core
   (:gen-class))
 
+(load "apriori")
+(load "bellNumbers")
+(load "decisionTree")
+(load "gradientDescent")
+(load "kmeans")
+(load "linearRegression")
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ; perform a linear regression where the vector [a b] returned describes a*x^0 + b*x^1 
+  (println (linear_regression [[0 0] [1 1] [2 2]] 0.03 [0 0] 1000)))
